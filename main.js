61bef1b9e469c3cfbb96cae96a411d85a3f5e594
oliveira-lucas7 <129890261+oliveira-lucas7@users.noreply.github.com>
2023-04-05 11:21:55 -0300

    evento nome

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 document.addEventListener("click" , showcolor );
 
+document.getElementById("botao").addEventListener("click" , shownome);
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
+}
+
+function shownome()
+{
+    let usuario = document.getElementById("nome").value
+    document.getElementById("mensagem").innerText = "Meu nome é: " + usuario
 }
 
